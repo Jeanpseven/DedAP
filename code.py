378ce1f536126414dbe7ed9c86611832c60bee24
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-26 21:01:18 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return app.send_static_file('index.html')
+    for filename in os.listdir('.'):
+        if filename.endswith('.html'):
+            return app.send_static_file(filename)
+    return 'No HTML files found in this directory', 404
+
+@app.route('/<path:path>', methods=['GET'])
+def get_html(path):
+    if path.endswith('.html'):
+        return send_from_directory('.', path)
+    return 'Not an HTML file', 400
 
 @app.route('/login', methods=['POST'])
 def login():
